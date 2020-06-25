b2f5fa1f84a7c7e90c063a7211ec1f52a88dbe73
kndwin <tnkevino@gmail.com>
2020-06-25 19:13:47 +1000

    Refactored routing code to match tests cases

--- routes.js
+++ routes.js
@@ -3,20 +3,100 @@ const shortid = require('shortid');
 
 module.exports = function(app) {
   var done = (err, data) => {
-    if (err) {
-      console.log("Error" + err);
-    } else {
-      console.log("Completed: " + data);
-    }
+    console.log( err ? "Error: " + error : "Success: " + data  );
   };
+  
+  // 1. I can create a user by posting form data username to 
+  // /api/exercise/new-user and returned will be an object
+  // with username and _id
+  
+  app.post("/api/exercise/new-user", function( req,res ) {
+    User.findOne({ username: req.body.username }).then( user => {
+      if (user !== null) {
+        return Promise.reject(new Error("The user already exist"));
+      } else {
+        const newUser = {
+          username: req.body.username,
+          userId: shortid.generate()
+        }
+        res.json({
+          username: req.body.username,
+          _id: newUser.userId
+        });
+        return User.create(newUser);
+      }
+    }).catch(err => done(err));
+  });
 
-  app.get( "/api/hello", (req, res, next) => {
-    res.json({
-      message: "Hello world!"
-    })
-  })
+  // 2. I can get an array of all users by getting
+  // api/exercise/users with the same info as when
+  // creating a user.
+  
+  app.get("/api/exercise/users", function (req,res) {
+    User.find({}, '-_id -__v -logs').then( users => {
+      res.json(users);
+    });
+  });
 
-  // GET response
+  // 3. I can add an exercise to any user by posting form 
+  // data userId(_id), description, duration, and optionally
+  // date to /api/exercise/add. If no data supplied it will
+  // user current data. Returned will be the user object with
+  // also with the exercise fields added.
+
+  app.post("/api/exercise/add", function( req, res ) {
+    User.findOne({ userId: req.body.userId })
+      .then(user => {
+        if (user === null) {
+          res.json("The user does not exist");
+        } else {
+          var { userId, username, description, 
+              duration, date } = req.body
+
+          if (date == "" || date == undefined) {
+            date = new Date();
+          }
+
+          if (userId === "") {
+            res.json( "Please enter a user ID" );
+          } else if (description === "") {
+            res.json( "Please enter a description" );
+          } else if (duration === "") {
+            res.json( "Please enter a duration" );
+          } else if (isNaN(parseInt(duration)) === true) {
+            res.json( "Please enter a duration with numbers" );
+          } else if (date == "Invalid Date" ) {
+            res.json("Please enter a valid date")
+          } else {
+
+            res.json({
+              username: username,
+              description: description,
+              duration: duration,
+              _id: userId,
+              date: date.toDateString()
+            });
+           
+            user.logs.push({
+              description: description,
+              duration: duration,
+              date: date
+            });
+            return user.save(done);
+          }
+        }
+      }).catch(err => done(err));
+  });
+  
+  // 4. I can retrieve a full exercise log of any user by 
+  // getting /api/exercise/log with a paramter of userId(_id)
+  // Return will be the user object with added array log and
+  // count (total exercise count)
+
+  // 5. I can retrieve part of the log of any user by also passing
+  // along optional paramters of from & to or limit
+  // (Date format yyyy-mm-dd, limit = int)
+
   app.get("/api/exercise/log", function( req,res ) {
     if (req.query.userId !== undefined) {
       User.find({ userId: req.query.userId}, 
@@ -56,80 +136,5 @@ module.exports = function(app) {
         .then( logs => { res.json( logs );
       }).catch(err => done(err));
     }
-  });
-
-  app.get("/api/exercise/users", function (req,res) {
-    User.find({}, '-_id -__v -logs').then( users => {
-      res.json(users);
-    });
-  });
-
-  // POST response
-  app.post("/api/exercise/new-user", function( req,res ) {
-    User.findOne({ username: req.body.username }).then( user => {
-      if (user !== null) {
-        return Promise.reject(new Error("The user already exist"));
-      } else {
-        const newUser = {
-          username: req.body.username,
-          userId: shortid.generate()
-        }
-        res.json({
-          username: req.body.username,
-          _id: newUser.userId
-        });
-        return User.create(newUser);
-      }
-    }).catch(err => done(err));
-  });
-
-  app.post("/api/exercise/add", function( req, res ) {
-    console.log("========================")
-    console.log("req=====================")
-    console.log("========================")
-    console.log(JSON.stringify(req.body, null, 2))
-    User.findOne({ userId: req.body.userId }).then(user => {
-      if (user === null) {
-        res.json("The user does not exist");
-        return Promise.reject(new Error("The user does not exist"));
-      } else {
-        var { userId, username, description, duration, date } = req.body
-
-        if (date == "" || date == undefined) {
-          date = new Date();
-        }
-
-        console.log(`Date after conversion: ${date}`)
-
-        if (userId === "") {
-          res.json( "Please enter a user ID" );
-        } else if (description === "") {
-          res.json( "Please enter a description" );
-        } else if (duration === "") {
-          res.json( "Please enter a duration" );
-        } else if (isNaN(parseInt(duration)) === true) {
-          res.json( "Please enter a duration with numbers" );
-        } else if (date == "Invalid Date" ) {
-          res.json("Please enter a valid date")
-        } else {
-          const log = {
-            description: description,
-            duration: duration,
-            date: date
-          };
-
-          res.json({
-            username: username,
-            description: description,
-            duration: duration,
-            _id: userId,
-            date: date
-          });
-         
-          user.logs.push(log);
-          return user.save(done);
-        }
-      }
-    }).catch(err => done(err));
   });
 }
